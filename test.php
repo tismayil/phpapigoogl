546ba200458f4e7396e6ee614f9ce8eecd9f133f
Ismayil Tahmazov <tehmezovismayil@gmail.com>
2018-08-14 11:16:33 +0300

    Update test.php

--- test.php
+++ test.php
@@ -3,6 +3,6 @@ require("goo.php");
 
 $goo = new gooGl(" YOUR_API_KEY ");
 
-print_r(  $goo->url("https://faydabul.com/index.php")->json(0)->get("short")  ); 
+print_r(  $goo->url("https://site.com")->json(0)->get("short")  ); 
 
 ?>
